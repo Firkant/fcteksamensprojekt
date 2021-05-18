fdeb38541dd36f0601a24169005cfb636773f6dd
Peter Lava Espersen <pede.13@live.dk>
2021-05-18 16:18:13 +0200

    commit2

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,13 @@
   <router-view />
 </template>
 
+
+
 <style lang="scss">
 #app {
+  margin: 0px;
+  width: 400px;
+  height: 400px;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -11,16 +16,4 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import {app, BrowserWindow} from "electron";
+
 
 createApp(App).use(router).mount('#app')
